61e4f816d9ad66afb96905cc43c20c639c19407c
Atauba Gideon <86924003+Gidii04@users.noreply.github.com>
2023-06-05 20:52:18 +0100

    add all files

--- app.js
+++ app.js
@@ -14,3 +14,32 @@ mobileBar.addEventListener('click', () => {
 closeBar.addEventListener('click', () => {
   mobileOverlay.classList.remove('show-mobile-overlay')
 })
+
+var startX
+var startY
+var swipeThreshold = 50
+
+document
+  .querySelector('.mobile-overlay')
+  .addEventListener('touchstart', function (event) {
+    startX = event.touches[0].clientX
+    startY = event.touches[0].clientY
+    // console.log(startX)
+  })
+
+document
+  .querySelector('.mobile-overlay')
+  .addEventListener('touchmove', function (event) {
+    var currentX = event.touches[0].clientX
+    var currentY = event.touches[0].clientY
+    var diffX = currentX - startX
+    var diffY = currentY - startY
+    // console.log(diffX)
+    // console.log(diffY)
+
+    if (Math.abs(diffX) > Math.abs(diffY) && diffX > swipeThreshold) {
+      document
+        .querySelector('.mobile-overlay')
+        .classList.remove('show-mobile-overlay')
+    }
+  })
